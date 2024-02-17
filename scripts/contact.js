d28fb947d318798659a776f2cbf1d37d12bb106d
fathima <fathima.bukhari@dcmail.ca>
2024-02-17 13:36:01 -0500

    week 6

---
+++ scripts/contact.js
@@ -0,0 +1,71 @@
+"use strict";
+
+(function (core) {
+
+    class Contact {
+
+        constructor(fullName = "", contactNumber = "", emailAddress = "") {
+            this._fullName = fullName;
+            this._contactNumber = contactNumber;
+            this._emailAddress = emailAddress;
+        }
+
+        get fullName() {
+            return this._fullName;
+        }
+
+        set fullName(value) {
+            this._fullName = value;
+        }
+
+        get contactNumber() {
+            return this._contactNumber;
+        }
+
+        set contactNumber(value) {
+            this._contactNumber = value;
+        }
+
+        get emailAddress() {
+            return this._emailAddress;
+        }
+
+        set emailAddress(value) {
+            this._emailAddress = value;
+        }
+
+        toString() {
+            return ` Full Name: ${this._fullName}\n 
+                    Contact Number: ${this._contactNumber}\n 
+                    Email Address: ${this._emailAddress}`
+        }
+
+        /**
+         Serialize for writing to local Storage
+         */
+        serialize() {
+            if (this._fullName !== "" && this._contactNumber !== "" && this._emailAddress !== "") {
+                return `${this.fullName}, ${this.contactNumber}, ${this.emailAddress}`;
+            }
+            console.error(`One or more of the contact properties is missing`);
+            return null;
+        }
+
+        /**
+         * Deserialize is used to read data from localStorage
+         */
+        deserialize(data) {
+
+            let propertyArray = data.split(",");
+            this._fullName = propertyArray[0];
+            this._contactNumber = propertyArray[1];
+            this._emailAddress = propertyArray[2];
+
+        }
+
+    }
+
+    //namespace definition
+    core.Contact = Contact;
+})(core || (core = {}));
+
